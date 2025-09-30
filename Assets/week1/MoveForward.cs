bdfbf07d2637d48bd9463dcedacca0afe3c28df5
tyceo <tyceo87@gmail.com>
2025-09-30 13:19:37 +1000

    Move forward and spawn in grid

--- Assets/week1/MoveForward.cs
+++ Assets/week1/MoveForward.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
+    public Rigidbody Rb;
+    public float zSpeed = 100f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveObjectForward();
+    }
+
+    public void MoveObjectForward()
+    {
+        Rb.AddRelativeForce(Vector3.forward * zSpeed);
+
     }
 }
--- Assets/week1/Spawner.cs
+++ Assets/week1/Spawner.cs
@@ -4,12 +4,17 @@ public class Spawner : MonoBehaviour
 {
     public GameObject prefab;
     public int amount;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float spacing = 2f; 
+    public int gridSize = 5;   
+    public Transform spawnOrigin;
+
+    
     void Start()
     {
         for (int i = 0; i < amount; i++)
         {
-            Instantiate(prefab, transform.position, Quaternion.identity);
+            //Instantiate(prefab, transform.position, Quaternion.identity);
+            SpawnInGrid();
         }
     }
 
@@ -17,6 +22,20 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         
+
+    }
+    void SpawnInGrid()
+    {
+        Vector3 startPos = spawnOrigin != null ? spawnOrigin.position : transform.position;
+
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int z = 0; z < gridSize; z++)
+            {
+                Vector3 position = startPos + new Vector3(x * spacing, 0, z * spacing);
+                Instantiate(prefab, position, Quaternion.identity);
+            }
+        }
 
     }
 }
