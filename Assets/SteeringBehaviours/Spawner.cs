fe09e80dc8a7d2df201e2705ab3d4130d40e722e
tyceo <tyceo87@gmail.com>
2025-10-14 11:49:03 +1000

    Zones and multiple NPCs created

--- Assets/SteeringBehaviours/Spawner.cs
+++ Assets/SteeringBehaviours/Spawner.cs
@@ -1,21 +1,20 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject prefab;
+    //public GameObject prefab;
     public int amount;
     public float spacing = 2f; 
     public int gridSize = 5;   
     public Transform spawnOrigin;
+    public List<GameObject> prefabs;
 
     
     void Start()
     {
-        for (int i = 0; i < amount; i++)
-        {
-            //Instantiate(prefab, transform.position, Quaternion.identity);
-            SpawnInGrid();
-        }
+        SpawnInGrid();
+        
     }
 
     // Update is called once per frame
@@ -33,9 +32,11 @@ public class Spawner : MonoBehaviour
             for (int z = 0; z < gridSize; z++)
             {
                 Vector3 position = startPos + new Vector3(x * spacing, 0, z * spacing);
-                Instantiate(prefab, position, Quaternion.identity);
+                //randomly select
+                GameObject selectedPrefab = prefabs[Random.Range(0, prefabs.Count)];
+                Instantiate(selectedPrefab, position, Quaternion.identity);
             }
         }
-
     }
+
 }
